8c9a23730be14f73221b35294578549940df2f69
Juwatomfri <Anyagavriliuk@gmail.com>
2024-10-25 22:14:58 +0300

    Добавлены билдеры

---
+++ Logic/Builders/AlbumBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Builders
+{
+    public class AlbumBuilder
+    {
+        private readonly Album _album;
+
+        public AlbumBuilder(string title, List<Singer> singers)
+        {
+            _album = new Album(title, singers);
+        }
+
+        public AlbumBuilder AddSong(Track track)
+        {
+            _album.Tracks.Add(track);
+            return this;
+        }
+
+        public Album Build() => _album;
+    }
+}
---
+++ Logic/Builders/SingerBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Builders
+{
+    public class SingerBuilder
+    {
+        private readonly Singer _singer;
+
+        public SingerBuilder(string name, Genre genre)
+        {
+            _singer = new Singer(name, genre);
+        }
+
+        public SingerBuilder AddTrack(Track track)
+        {
+            _singer.Tracks.Add(track);
+            return this;
+        }
+        
+        public SingerBuilder AddAlbum(Album album)
+        {
+            _singer.Albums.Add(album);
+            return this;
+        }
+
+        public Singer Build() => _singer;
+    }
+}
