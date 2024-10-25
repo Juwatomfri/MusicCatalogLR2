c072b1969a33c1f8fa0afb6bb5749535e943bee7
Juwatomfri <Anyagavriliuk@gmail.com>
2024-10-25 18:54:28 +0300

    Добавлена библиотека классов с классом для реализации поиска

---
+++ SearchInterface/SearchOption.cs
@@ -0,0 +1,29 @@
+﻿using Logic;
+
+namespace SearchInterface
+{
+    public class SearchOption
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+
+        public SearchOption(string whatToSearch)
+        {
+            switch (whatToSearch)
+            {
+                case "SingerSearch":
+                    Name = "Поиск по артистам";
+                    Value = new SingerSearchStrategy();
+                    break;
+                case "AlbumSearch":
+                    Name = "Поиск по альбомам";
+                    Value = new AlbumSearchStrategy();
+                    break;
+                case "TrackSearch":
+                    Name = "Поиск по трекам";
+                    Value = new TrackSearchStrategy();
+                    break;
+            }
+        }
+    }
+}
